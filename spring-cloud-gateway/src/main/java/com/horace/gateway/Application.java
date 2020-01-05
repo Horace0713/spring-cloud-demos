763630be78762df6a85d78dc5c50747a5d611083
Horace0713 <dreamygh@163.com>
2020-01-05 10:43:44 +0800

    gateway 第一个转发

---
+++ spring-cloud-gateway/src/main/java/com/horace/gateway/Application.java
@@ -0,0 +1,17 @@
+package com.horace.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Horace
+ * @desc:
+ * @project: spring-cloud-demos
+ * @create: 2020-01-05 10:21
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
